2df5c422d5c8ecfd3c975a6108f93fa797613a91
Daniella Marques <86430405+daniellamarques@users.noreply.github.com>
2021-09-08 18:50:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,65 @@
-function leNotas () {
-  const notas = []
-  for (let index = 0; index < 3; index++) {
-    const nota = Math.random() * 10
-    console.log('nota ', index, ': ', nota)
-    notas = []
-  }
-  return notas
-}
+var select = document.getElementById("estados"); 
+var cidade = document.getElementById("cidade"); 
+const butaoSalvar = document.getElementById("salvar")
 
-function calculaMedia (notas) {
-  let soma = 0
-  for (let index = 0; index < notas.length; index++) {
-    soma = soma + notas[index]
-  }
-  const media = soma / notas.length
-  return media
-}
+var options = ["São Paulo", "Rio de Janeiro", "Ceará"]; 
+var cidade1 = ["São Paulo", "Itápolis", "Araraquara", "Ribeirão Preto", "Jacareí"];
+var cidade2 = ["Rio de Janeiro", "Niteroi", "Petropolis", "Belford Roxo", "Nova Iguaçu"];
+var cidade3 = ["Fortaleza", "Sobral", "Iguatu", "Mombaça", "Parambu"];
 
-function vericaSituacaoDoAluno (media) {
-  if (media < 4) {
-    return 'Reprovado'
-  } else if (media < 7) {
-    return 'Recuperação'
-  } else {
-    return 'Aprovado'
-  }
-}
 
-const notas = leNotas()
-const media = calculaMedia(notas)
-console.log('A sua média final é: ', media)
-const situacaoDoAluno = vericaSituacaoDoAluno(media)
-console.log('Situação do aluno: ', situacaoDoAluno)
+for(var i = 0; i < options.length; i++) {
+    var opt = options[i];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    //console.log(el)
+    select.appendChild(el);
+} 
+myFunction  = function (){
+	if(select){
+      var t = document.getElementById("estados");
+      var selectedText = t.options[t.selectedIndex].text;
+
+      if(selectedText == 'São Paulo'){
+          for(var j = 0; j < cidade1.length; j++) {
+              var opt = cidade1[j];
+              var el = document.createElement("option");
+              el.textContent = opt;
+              el.value = opt;
+              cidade.appendChild(el);
+          }   
+      }
+      if(selectedText == 'Rio de Janeiro'){
+        for(var j = 0; j < cidade2.length; j++) {
+            var opt = cidade2[j];
+            var el = document.createElement("option");
+            el.textContent = opt;
+            el.value = opt;
+            cidade.appendChild(el);
+        }   
+    }
+    if(selectedText == 'Ceará'){
+        for(var j = 0; j < cidade3.length; j++) {
+            var opt = cidade3[j];
+            var el = document.createElement("option");
+            el.textContent = opt;
+            el.value = opt;
+            cidade.appendChild(el);
+        }   
+    }
+    const select1 = document.getElementById("estados")
+    select1.addEventListener('change', () => {
+        estado = select1.options[select1.selectedIndex].text;
+    })
+    
+    const select2 = document.getElementById("cidade")
+    select2.addEventListener('change', () => {
+        cidade = select2.options[select2.selectedIndex].text;
+    })
+    
+    butaoSalvar.addEventListener('click', () => {
+        alert("UF: " + select + " - Cidade: " + cidade + ", Salvo com sucesso!");
+    })
+}
+}
